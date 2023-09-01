b6405cd1179569ab16de8283d7372eece2bc3b4d
Kingsley Onuwa <kingsleyonuwa27@gmail.com>
2023-09-01 21:24:30 +0100

    Mosh Vite + React Tutorial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./components/Message";
+import ListGroup from "./components/ListGroup"
+
+function App() {
+  return (
+    <>
+      <Message />
+      <ListGroup />
+    </>
+  )
+};
+
+export default App;
