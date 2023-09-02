352baf65ecdb63d84a97f46a7aed9b674cf1335b
Kingsley Onuwa <kingsleyonuwa27@gmail.com>
2023-09-02 03:25:39 +0100

    Mosh Vite + React Tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import NavBar from "./components/NavBar";
 import Message from "./components/Message";
 import ListGroup from "./components/ListGroup"
+import "./App.css"
 
 function App() {
   return (
     <>
+      <NavBar />
       <Message />
       <ListGroup />
     </>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+
+function NavBar() {
+  return (
+    <div className="Nav-bar">
+      <img src="" alt="logo" />
+    </div>
+  )
+}
+
+export default NavBar;
