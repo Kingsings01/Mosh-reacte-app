2274314060344378b682f0114192e6d9d47467d9
Kingsley Onuwa <kingsleyonuwa27@gmail.com>
2023-09-11 07:07:35 +0100

    Vite + React App of frontnow.ai

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 function NavBar() {
   return (
-    <div className="Nav-bar">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" alt="logo" className="webLogo" />
-      </a>
-      <button className="Resources-btn">Resources</button>
-      <button className="DemoBtn">Book a Demo</button>
-    </div>
+    <>
+      <div className="Nav-bar">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" alt="logo" className="webLogo" />
+        </a>
+        <button className="Resources-btn">Resources</button>
+        <button className="DemoBtn">Book a Demo</button>
+      </div>
+    </>
   );
 }
 
